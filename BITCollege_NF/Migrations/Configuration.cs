d84718064992e4bb184b6197554d4a0d54f69ff5
Nicholas Funk <nfunk4@academic.rrc.ca>
2024-02-26 09:46:01 -0600

    -Database Initial Migration. -NF

---
+++ BITCollege_NF/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace BITCollege_NF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BITCollege_NF.Data.BITCollege_NFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BITCollege_NF.Data.BITCollege_NFContext";
+        }
+
+        protected override void Seed(BITCollege_NF.Data.BITCollege_NFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
