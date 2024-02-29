db5460936e91826f898e4780d1c0c9306393b00e
Nicholas Funk <nfunk4@academic.rrc.ca>
2024-02-28 21:51:01 -0600

    -Added StoredProcedures migrations.
    -Updated database migrations.

---
+++ BITCollege_NF/Migrations/202402290348298_StoredProcedures.cs
@@ -0,0 +1,20 @@
+﻿namespace BITCollege_NF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StoredProcedures : DbMigration
+    {
+        public override void Up()
+        {
+            // Call script to create the stored procedure
+            this.Sql(Properties.Resources.create_next_number);
+        }
+        
+        public override void Down()
+        {
+            // Call script to drop the stored procedure
+            this.Sql(Properties.Resources.drop_next_number);
+        }
+    }
+}
