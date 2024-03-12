e42c2501dd4280f4cf0dc4bb35a3865ceeadf7bc
Nicholas Funk <nfunk4@academic.rrc.ca>
2024-03-12 11:50:11 -0500

    -Deleted IService1.cs, Service1.cs, Service1.svc.cs. -Created new WCF Service, CollegeRegistration along with three autogenerated files.

---
+++ BITCollegeService/CollegeRegistration.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BITCollegeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CollegeRegistration" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CollegeRegistration.svc or CollegeRegistration.svc.cs at the Solution Explorer and start debugging.
+    public class CollegeRegistration : ICollegeRegistration
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
