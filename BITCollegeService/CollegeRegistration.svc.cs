fd6d721948375a5e256108dea653c9cf945338a5
Nicholas Funk <nfunk4@academic.rrc.ca>
2024-03-13 20:01:04 -0500

    -Implemented DropCourse query.

--- BITCollegeService/CollegeRegistration.svc.cs
+++ BITCollegeService/CollegeRegistration.svc.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BITCollege_NF.Data;
+using BITCollege_NF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -11,13 +13,31 @@ namespace BITCollegeService
     // NOTE: In order to launch WCF Test Client for testing this service, please select CollegeRegistration.svc or CollegeRegistration.svc.cs at the Solution Explorer and start debugging.
     public class CollegeRegistration : ICollegeRegistration
     {
+        BITCollege_NFContext db = new BITCollege_NFContext();
+
         public void DoWork()
         {
         }
 
         public bool DropCourse(int registrationId)
         {
-            throw new NotImplementedException();
+            // Queries for one registrationId.
+            Registration registration = db.Registrations.Where(x => x.RegstrationId == registrationId).SingleOrDefault();
+            try
+            {
+                if (registration != null)
+                {
+                    db.Registrations.Remove(registration);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
         }
 
         public int RegisterCourse(int studentId, int courseId, string notes)
