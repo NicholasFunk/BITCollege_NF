5c778ae8b91a15c831aacdb2bca215296ed429d6
Nicholas Funk <nf4313@hotmail.com>
2024-03-27 21:16:17 -0500

    -Created auto-implemented properties in the constructor data class.

--- BITCollegeWindows/ConstructorData.cs
+++ BITCollegeWindows/ConstructorData.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using BITCollege_NF.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BITCollegeWindows
 {
@@ -13,5 +17,48 @@ namespace BITCollegeWindows
     /// </summary>
     public class ConstructorData
     {
+        // Pass student related information to the form.
+        public int StudentId { get; set; }
+
+        public int GradePointStateId { get; set; }
+
+        public int? AcademicProgramId { get; set; }
+
+        public long StudentNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public double? GradePointAverage { get; set; }
+        public double OutstandingFees { get; set; }
+        public string Notes { get; set; }
+
+        public string FullName { get; }
+
+        public string FullAddress { get; }
+
+        // Pass registration related information to the form.
+        public int RegstrationId { get; set; }
+
+        public int RegistrationStudentId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public long RegistrationNumber { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public double? Grade { get; set; }
+
+        public string RegistrationNotes { get; set; }
+
     }
 }
